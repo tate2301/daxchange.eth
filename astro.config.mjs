287c585e682b3bb2f1b68b60198fe48cc17a820f
Kamfeskaya <tatendachris@gmail.com>
2023-02-06 18:22:35 +0200

    feat: add partytown + analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind(), partytown()]
 });

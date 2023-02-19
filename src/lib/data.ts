354aaca39f6447530dafea52c880e80feeee7a4d
Kamfeskaya <tatendachris@gmail.com>
2023-02-19 17:32:38 +0200

    simplify

---
+++ src/lib/data.ts
@@ -0,0 +1,28 @@
+export const data = {
+  projects: [
+    {
+      name: "Telescope",
+      description:
+        "I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of.",
+      href: "https://telescope.kamfeskaya.com",
+      icon: "/projects/telescope.webp",
+      domain: "telescope.kamfeskaya.com",
+    },
+    {
+      name: "Paynow React",
+      description:
+        "A React wrapper for the Paynow SDK allowing you to use Paynow on client-side",
+      href: "/projects/paynow-react",
+      icon: "/projects/paynow-react.jpg",
+      domain: "github.com",
+    },
+    {
+      name: "Zicheni",
+      description:
+        "The Swiss pocket knife for building, running and scaling your SaaS startup.",
+      href: "https://zicheni.io",
+      icon: "/projects/saas-utils.png",
+      domain: "zicheni.io",
+    },
+  ],
+};
